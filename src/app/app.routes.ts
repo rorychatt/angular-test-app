325c94c9bfbc59af5b463b36286e3239aa163949
rorychatt <pavel.rinne@gmail.com>
2024-10-14 13:12:22 +0200

    feat: use router-outlet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    loadComponent: async () => {
+      const module = await import('./home/home.component');
+      return module.HomeComponent;
+    },
+  },
+];
