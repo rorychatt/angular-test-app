ab957f230f0f9da9eee6accaa3cdec6ec07d2742
rorychatt <pavel.rinne@gmail.com>
2024-10-14 22:14:08 +0200

    feat: use filter with pipe

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -4,10 +4,11 @@ import { Todo } from '../model/todo.type';
 import { catchError } from 'rxjs';
 import { TodoItemComponent } from '../components/todo-item/todo-item.component';
 import { FormsModule } from '@angular/forms';
+import { FilterTodosPipe } from '../pipes/filter-todos.pipe';
 @Component({
   selector: 'app-todos',
   standalone: true,
-  imports: [TodoItemComponent, FormsModule],
+  imports: [TodoItemComponent, FormsModule, FilterTodosPipe],
   templateUrl: './todos.component.html',
   styleUrl: './todos.component.scss',
 })
