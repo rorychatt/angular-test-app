b65a5dec9bf44352acab1b775e2750406844de30
rorychatt <pavel.rinne@gmail.com>
2024-10-14 12:40:00 +0200

    feat: pass through event button

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,8 +6,11 @@ import { GreetingComponent } from '../components/greeting/greeting.component';
   standalone: true,
   imports: [GreetingComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+  keyUpHandler(event: KeyboardEvent) {
+    console.log('User pressed key:', event.key);
+  }
   homeMessage = signal('Hello, World!');
 }
