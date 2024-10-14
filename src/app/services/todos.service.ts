f6774a4600186e24bc1811248738a4853b6fd069
rorychatt <pavel.rinne@gmail.com>
2024-10-14 16:25:15 +0200

    feat: styling for todoitems list

--- src/app/services/todos.service.ts
+++ src/app/services/todos.service.ts
@@ -1,29 +1,15 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { Todo } from '../model/todo.type';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodosService {
-  todoItems: Array<Todo> = [
-    {
-      userId: 0,
-      completed: false,
-      title: 'Task to do',
-      id: 0
-    },
-    {
-      userId: 0,
-      completed: false,
-      title: 'Another task to do',
-      id: 1
-    },
-    {
-      userId: 0,
-      completed: false,
-      title: 'Oops forgot about this',
-      id: 2
-    }
-  ]
-  constructor() { }
+  http = inject(HttpClient)
+  
+  getTodosFromApi() {
+    const url = 'https://jsonplaceholder.typicode.com/todos';
+    return this.http.get<Array<Todo>>(url);
+  }
 }
