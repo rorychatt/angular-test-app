ad447a49567fa75f5218bea9ea6b4283dc0f3556
rorychatt <pavel.rinne@gmail.com>
2024-10-14 22:12:23 +0200

    feat: declare filter as pipe

--- src/app/pipes/filter-todos.pipe.ts
+++ src/app/pipes/filter-todos.pipe.ts
@@ -1,4 +1,5 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Todo } from '../model/todo.type';
 
 @Pipe({
   name: 'filterTodos',
@@ -6,8 +7,16 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FilterTodosPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(todos: Todo[], searchTerm: string): Todo[] {
+    if(!searchTerm) {
+      return todos;
+    }
+
+    const text = searchTerm.toLowerCase();
+
+    return todos.filter((todo) => {
+      return todo.title.toLowerCase().includes(text);
+    });
   }
 
 }
