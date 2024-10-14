167143a6efeed9b5a7faf40be63c3cff758a843a
rorychatt <pavel.rinne@gmail.com>
2024-10-14 12:19:59 +0200

    feat: fix greeting component import

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { GreetingComponent } from '../components/greeting/greeting.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [GreetingComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
