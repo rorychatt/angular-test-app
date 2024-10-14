30281625a0273e43c342353c9cd462f865c1ab5c
rorychatt <pavel.rinne@gmail.com>
2024-10-14 13:39:20 +0200

    feat: add template data in todos service

--- src/app/services/todos.service.ts
+++ src/app/services/todos.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { Todo } from '../model/todo.type';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodosService {
-
+  todoItems: Array<Todo> = [
+    {
+      userId: 0,
+      completed: false,
+      title: 'Task to do',
+      id: 0
+    },
+    {
+      userId: 0,
+      completed: false,
+      title: 'Another task to do',
+      id: 1
+    },
+    {
+      userId: 0,
+      completed: false,
+      title: 'Oops forgot about this',
+      id: 2
+    }
+  ]
   constructor() { }
 }
